0872fcfc7ce193b3021c24dec8bff0cc5c6685b8
Arman <161294136+Armanmurad123@users.noreply.github.com>
2024-05-31 12:27:57 +0500

    rtgg

---
+++ app.js
@@ -0,0 +1,10 @@
+function showAlert() {
+    alert("This is an alert message by a js coding!");
+}
+
+function askName() { 
+    const name = prompt("What is your name?");
+    if (name) {
+        alert("Hello, " + name + "!");
+    }
+}
